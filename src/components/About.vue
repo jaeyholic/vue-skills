89f2c7340f9fb9f735527c2c6b5a4b7255aab4b6
Felix Nana Yaw Yeboah-Jefferson <32690370+jaeyholic@users.noreply.github.com>
2018-04-02 01:43:03 +0000

    Initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+      <h1>This is an about page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati architecto officia veniam, minima nam eum temporibus dolore necessitatibus quam ipsam, vero adipisci suscipit est eveniet eos, voluptate totam odio labore.</p>
+  </div>
+</template>
